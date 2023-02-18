b0d111351e32556cbea4227420f2f49f17a0ad32
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-02-18 18:21:42 +0530

    wallet added

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,36 @@
-
 import BasicTabs from "./TabPanel";
 import WalletConnector from "./WalletConnector";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Auth, useAuth } from "@arcana/auth-react";
+
+const onLogin = () => {
+  // Route to authenticated page
+  alert("Logged In");
+};
 
 const darkTheme = createTheme({
-    palette: {
-        mode: 'light',
-    },
+  palette: {
+    mode: "light",
+  },
 });
 
 export default function App() {
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
-            <BasicTabs />
-            <WalletConnector />
-        </ThemeProvider>
-    )
+  const auth = useAuth();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      {auth.loading ? (
+        "Loading"
+      ) : auth.isLoggedIn ? (
+        <p>Logged In</p>
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"light"} onLogin={onLogin} />
+        </div>
+      )}
+      <CssBaseline />
+      <BasicTabs />
+      <WalletConnector />
+    </ThemeProvider>
+  );
 }
