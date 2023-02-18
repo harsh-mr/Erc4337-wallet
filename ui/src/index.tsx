bb6cceeef8f044b5e68c28d1757aceac8ad308e7
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-02-19 00:24:00 +0530

    added wallet configs

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,14 +1,34 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { render } from "react-dom";
-import { AuthProvider } from "@arcana/auth";
+import { AuthProvider, CHAIN } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 import App from './App';
 
 const appAddress = "f162a4f56924d1c356c05266b09ddb76500d6da9"
-const provider = new AuthProvider(`${appAddress}`) // required
+const auth = new AuthProvider(`${appAddress}`, { //required
+  network: 'testnet', //defaults to 'testnet'
+  // position: 'right', //defaults to right
+  theme: 'dark', //defaults to dark
+  alwaysVisible: true, //defaults to true which is Full UI mode
+  chainConfig: {
+    chainId: CHAIN.ETHEREUM_GOERLI, //defaults to CHAIN.ETHEREUM_MAINNET
+    rpcUrl: 'https://side-divine-patina.ethereum-goerli.discover.quiknode.pro/da5d7e1f010d66a6c052c2aac2bb66b14516754d/', //defaults to 'https://rpc.ankr.com/eth'
+  },
+}) // required
+const initAuth = async () => {
+  await auth.init()
+}
+try {
+  initAuth();
+} catch (e) {
+  // Handle exception case
+  console.error(e)
+  alert("An error occurred")
+}
+
 render(
-  <ProvideAuth provider={provider}>
+  <ProvideAuth provider={auth}>
     <App />,
   </ProvideAuth>,
   document.getElementById("root")
