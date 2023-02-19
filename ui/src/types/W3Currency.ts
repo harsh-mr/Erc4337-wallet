68de54d32623aa22f9f3a8d834f93e18732f31a3
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-02-19 23:44:43 +0530

    done

---
+++ ui/src/types/W3Currency.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const W3Currency = () => {
+  return (
+    <div>W3Currency</div>
+  )
+}
+
+export default W3Currency
---
+++ ui/src/web3API/constants.ts
@@ -0,0 +1,30 @@
+import { W3Currency } from '../types'
+
+export const ensUri = 'ens/v2.uniswap.web3api.eth'
+
+export const ETHER: W3Currency = {
+  decimals: 18,
+  name: 'Ether',
+  symbol: 'ETH'
+}
+
+export const networks: any = {
+  '1': {
+    chainId: 1,
+    name: 'mainnet',
+    node: 'https://mainnet.infura.io/v3/b76cba91dc954ceebff27244923224b1',
+    explorer: 'https://etherscan.io'
+  },
+  '3': {
+    chainId: 3,
+    name: 'ropsten',
+    node: 'https://ropsten.infura.io/v3/b76cba91dc954ceebff27244923224b1',
+    explorer: 'https://ropsten.etherscan.io'
+  },
+  '4': {
+    chainId: 4,
+    name: 'rinkeby',
+    node: 'https://rinkeby.infura.io/v3/b76cba91dc954ceebff27244923224b1',
+    explorer: 'https://rinkeby.etherscan.io'
+  }
+}
